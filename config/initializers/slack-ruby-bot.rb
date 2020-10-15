2a1a099cb91bb446bba6db878d610e83f7a0b15b
pyama86 <pyama@pepabo.com>
2020-10-15 09:10:15 +0900

    allow bot message

--- config/initializers/slack-ruby-bot.rb
+++ config/initializers/slack-ruby-bot.rb
@@ -5,6 +5,7 @@ end
 SlackRubyBot::Client.logger.level = Logger::INFO
 SlackRubyBot.configure do |config|
   config.allow_message_loops = false
+  config.allow_bot_messages = true
 end
 
 App::Registry.register(:bot_token_client, Slack::Web::Client.new(token: ENV["SLACK_API_TOKEN"]))
