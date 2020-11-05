b7814a0d2f6531733af9d9e1f6a9bf4fff56a697
pyama86 <pyama@pepabo.com>
2020-11-05 10:17:26 +0900

    support word sepalater

--- app/models/security_group.rb
+++ app/models/security_group.rb
@@ -2,7 +2,7 @@ module App
   module Models
     class SecurityGroup < SlackRubyBot::MVC::Model::Base
       def params(text)
-        text.match(/(?<action>[^\s]+)\s+(?<id_or_name>[^\s]+)\s?(?<tenant>[^\s]+)?$/i)
+        text.match(/(?<action>[^\b\s]+)[\b\s]+(?<id_or_name>[^\s\b]+)[\b\s]?(?<tenant>[^\s\b]+)?$/i)
       end
     end
   end
